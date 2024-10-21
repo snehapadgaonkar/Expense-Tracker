2286b3b161ebc192568c4ffe9989543a6fc8e3e7
snehapadgaonkar <padgaonkarsneha@gmail.com>
2024-10-22 00:20:42 +0530

    added files

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+const colors = require('colors')
+const connectDb = require('./config/connectDb')
+
+//config dot env file 
+dotenv.config();
+
+//database call
+connectDb();
+
+//rest object
+const app = express()
+
+//middlewares
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(cors())
+
+//routes
+app.get('/',(req,res) => {
+    res.send('<h1>Hello from server</h1>')
+})
+
+//port
+const PORT = 8080 || process.env.PORT
+
+//listen server
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
