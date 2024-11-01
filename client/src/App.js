29c97f2164eb29f5fcea3d6f1651a9ee74601498
snehapadgaonkar <padgaonkarsneha@gmail.com>
2024-11-02 00:21:42 +0530

    added register and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
-import {Routes,Route} from 'react-router-dom'
-import HomePage from './pages/HomePage';
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path='/' element={<HomePage/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </>
   );
 }
